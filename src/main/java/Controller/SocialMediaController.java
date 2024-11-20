8459a6011692d7bd909e5bdc192e9fd243903836
Sytiva Wheeler <sytivawheelercareers@gmail.com>
2024-11-20 22:34:54 +0000

    Updated Files

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -9,17 +9,14 @@ import io.javalin.http.Context;
 import Model.Account;
 import Model.Message;
 
-//import Service.AccountService;
-
+import Service.AccountService;
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
  * found in readme.md as well as the test cases. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
-    //AccountService accountService;
-    //MessageService messageService;
-
+    AccountService acc = new AccountService();
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -48,7 +45,7 @@ public class SocialMediaController {
     private void postAccountHandler(Context ctx) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         Account account = mapper.readValue(ctx.body(), Account.class);
-        Account addedAccoount = 
+        //Account addedAccoount = 
 
     }
 
